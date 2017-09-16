4e414a8af7d29ac01547307ada565b5bfda1c2bc
Veeral Bhagat <bhagatv@uci.edu>
2017-09-15 17:13:37 -0700

    second commit

---
+++ StreamableAPI/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+  name = 'PyStreamableCom',
+  # this must be the same as the name above
+  version = '1.1',
+  description = 'A fully functional Python API for Streamable.com',
+  author = 'Veeral Bhagat',
+  author_email = 'kurv.100@gmail.com',
+  url = 'https://github.com/Bhagatv/PyStreamableCom', # use the URL to the github repo
+  download_url = 'https://github.com/Bhagatv/PyStreamableCom/archive/1.0.tar.gz', # I'll explain this in a second
+  keywords = ['Streamable.com', 'wrapper', 'Python'], # arbitrary keywords
+  classifiers = []
+  
+)
+
+
+
+'''[distutils]
+index-servers =
+  pypi
+  pypitest
+
+[pypi]
+repository=https://pypi.python.org/pypi
+username=blirm
+password=11335577Lol
+
+[pypitest]
+repository=https://testpypi.python.org/pypi
+username=11335577Lol
+password=11335577Lol'''
+
+#in pypirc file
